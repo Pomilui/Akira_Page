1d5e7533d6c383c02a2a2ce67f0de8c5e619ded9
Pcsolar <NPP16@pitt.edu>
2018-04-25 14:41:54 -0400

    Adding changes to checkBox javascript file.

--- Javascript/checkBox.js
+++ Javascript/checkBox.js
@@ -4,61 +4,14 @@ window.onload = init;
 
 
 function init() {
-    var fieldset = document.getElementsByTagName('input');
-    for (var i = 0; i < fieldset.length; i++) {
-        fieldset[i].addEventListener('click', changeColor, false);
+var buttons = document.getElementsByTagName("button")
+buttons[0].addEventListener('click', changeColor,false)
     }
-}
 
-function changeColor() {
-var p1 = document.getElementById("colorToggle")
-{p1.style.backgroundColor = "skyblue";}
-}
 
-function toggleInput() {
-    var id = this.id;
-    switch (id) {
-        case "controlSceneToggle": {
-            var commas = document.getElementsByClassName("controlScene");
-            for (var i = 0; i < commas.length; i++) {
-                commas[i].classList.toggle("on")
-            }
-        };
-        break;
-        case "locToggle": {
-            var periods = document.getElementsByClassName("loc");
-            for (var i = 0; i < periods.length; i++) {
-                periods[i].classList.toggle("on")
-            }
-        };
-        break;
-        case "currentToggle": {
-            var semicolons = document.getElementsByClassName("current");
-            for (var i = 0; i < semicolons.length; i++) {
-                semicolons[i].classList.toggle("on")
-            }
-        };
-        break;
-        case "dateToggle": {
-            var colons = document.getElementsByClassName("date");
-            for (var i = 0; i < colons.length; i++) {
-                colons[i].classList.toggle("on")
-            }
-        };
-        break;
-        case "eventToggle": {
-            var colons = document.getElementsByClassName("event");
-            for (var i = 0; i < colons.length; i++) {
-                colons[i].classList.toggle("on")
-            }
-        };
-        break;
-        case "companyToggle": {
-            var open_p = document.getElementsByClassName("company");
-            for (var i = 0; i < open_p.length; i++) {
-                open_p[i].classList.toggle("on")
-            }
-        };
-        break;
-    }
+function changeColor2() {
+var pars = document.getElementsByTagName('spGrp')
+    for (var i = 0, length = pars.length; i < length; i++) {
+        pars[i].style.backgroundColor = "skyblue";
+    }   
 }
